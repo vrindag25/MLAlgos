6d480eab46ae79f77af8bc4c12dfb81b81363b2f
Vrinda Gupta <vrinda2592@gmail.com>
2020-02-25 14:44:49 +0530

    Extract important features from PCA loadings

--- pca.py
+++ pca.py
@@ -9,9 +9,20 @@ sklearn_pca.fit(data(var_list])
 sklearn_transf = sklearn_pca.fit_transform(data[var_list])
 
 # Loadings ( http://www.nxn.se/valent/loadings-with-scikit-learn-pca)
-loadings = pd.DataFrame(sklearn_pca.components_)
-loadings_T = loadings.T
+loadings_df = pd.DataFrame(sklearn_pca.components_.T)
+loadings_df.index = X.columns
+loadings_df.reset_index(inplace = True)
+loadings_df
 
-loadings_T['Feature']= x_var
-loadings_T.columns = ['PC1','PC2','PC3','PC4','PC5','Feature']
-loadings_T.sort_values(['PC1','PC2', 'PC3', 'PC4'], ascending = False)
+# Take absolute values
+loadings_df.iloc[:,1:] = loadings_df.iloc[:,1:].apply(lambda x:abs(x), axis = 0)
+#To find important features based on loadings
+imp_feat = list(loadings_df.sort_values(by = 0, ascending = False)['index'][:40])
+imp_feat = imp_feat + list(loadings_df.sort_values(by = 1, ascending = False)['index'][:40])
+imp_feat = imp_feat + list(loadings_df.sort_values(by = 2, ascending = False)['index'][:40])
+imp_feat = imp_feat + list(loadings_df.sort_values(by = 3, ascending = False)['index'][:20])
+imp_feat = imp_feat + list(loadings_df.sort_values(by = 4, ascending = False)['index'][:20])
+imp_feat = list(dict.fromkeys(imp_feat))
+
+for feat in imp_feat:
+    print(feat, end = "','") 
