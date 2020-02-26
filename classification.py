b2262960025215be8a35c451ebf6f64e35a6bd3e
Vrinda Gupta <vrinda2592@gmail.com>
2020-02-26 15:52:33 +0530

    PR Curve

--- classification.py
+++ classification.py
@@ -8,3 +8,19 @@ def performance_report(y_test,pred_proba, cutoff, flag= True):
     conf_mat = confusion_matrix(y_test,np.array(pred_proba > cutoff))
     report = classification_report(y_test, pred_proba > cutoff)
     return auc_roc1,conf_mat,report
+
+precision, recall, thresholds = precision_recall_curve(y_test, xgb_pred_prob[:,1])
+
+############ Precision Recall Curve ############
+# # calculate precision-recall AUC
+auc_roc1 = roc_auc_score(y_test,
+              np.array(xgb_pred_prob[:,1] > 0.3))
+
+print('AUC ROC', auc_roc1)
+
+# plot the precision-recall curve for the model
+plt.plot(thresholds,precision[:-1],  color = 'g', 
+         label = 'Precision')
+plt.plot( thresholds, recall[:-1],linestyle = '--', color = 'b', label = 'Recall')
+plt.xlabel("Threshold")
+plt.legend(loc = 'lower left')
