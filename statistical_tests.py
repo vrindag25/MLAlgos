e742e7c7d4e704f6c50157cb80967bdfb40df011
Vrinda Gupta <vrinda2592@gmail.com>
2020-02-29 09:26:48 +0530

    Update statistical_tests.py

--- statistical_tests.py
+++ statistical_tests.py
@@ -26,4 +26,39 @@ for col in data[num_vars]:
     res2 = kruskal(x,y)
     p_value_table.loc[i, 'kruskal_pvalue'] = round(res2.pvalue,4)
     
+    
+  #################SKLEARN########################
+
+# Model initialization
+regression_model = LinearRegression()
+# Fit the data(train the model)
+regression_model.fit(x1, y)
+# Predict
+y_predicted = regression_model.predict(x1)
+
+##############StatsModel#######################
+newX = sm.add_constant(x1)
+regression_model_sm = sm.OLS(y, newX)
+regression_model_sm_fit = regression_model_sm.fit()
+print(regression_model_sm_fit.summary())
+
+print("------------------Variation Explained by Coefficients -----------------")
+beta_coef = regression_model.coef_
+alpha_coef = regression_model.intercept_
+
+print(Fore.GREEN +'Variation Explained by alpha: ',
+      alpha_coef[0]/y.mean())
+
+print(Fore.GREEN +'Variation Explained by beta: ',
+      beta_coef[0,0]*x1.mean()/y.mean()) # ,beta_coef[0,1]*x2.mean()/y.mean())
+# plotting values
+
+# data points
+plt.scatter(x1, y) #, s=10
+plt.xlabel('Program Utilisation')
+plt.ylabel('Reversal Rate')
+
+# predicted values
+plt.plot(x1, y_predicted, color='r')
+plt.show()
     i = i+1
